e6b42832c389ae5c70d7d0d2fef4b2cb553f6fe2
murat <murat@suade.org>
2020-12-09 14:07:55 +0000

    More ex

--- utils/example.py
+++ utils/example.py
@@ -1,13 +1,17 @@
 
 
 def create_csv():
-    my_function(3)
+    my_function(3.4)
 
 
 def my_function(x):
     """
     We want x to get squared
     """
+    if not isinstance(x, int) and not isinstance(x, float):
+        print("got an error, oops, do nothing")
+        return
+
     # comment
     new_x = x * x
     print(f"{x} became {new_x}")
@@ -19,5 +23,11 @@ def write_result_to_csv(new_x, x):
         file.write("original,new value,description,\n")
         file.write(f"{x},{new_x},it is squared!,\n")
 
+def mytest():
+    my_function(3)
+    my_function(3.4)
+    my_function("sarah")
+
 if __name__ == "__main__":
+    mytest()
     create_csv()
